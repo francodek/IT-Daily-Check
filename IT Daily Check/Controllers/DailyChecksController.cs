e8c068718a31555ac0862cbfabbd52281ee2fff9
fisayoadegun <fisayoadegun@gmail.com>
2023-08-14 08:39:25 +0100

    updated email sender

--- IT Daily Check/Controllers/DailyChecksController.cs
+++ IT Daily Check/Controllers/DailyChecksController.cs
@@ -283,9 +283,11 @@ namespace IT_Daily_Check.Controllers
 
             string viewHtml = await RenderViewToStringAsync("EmailTemplate", dailyCheck);
             var email = new MimeMessage();
-           
+            email.From.Add(new MailboxAddress($"{user.FirstName} {user.LastName}", "Auto.Mail@gmt-limited.com"));
             email.To.Add(MailboxAddress.Parse("itgroup@gmt-limited.com"));
-            email.Cc.Add(MailboxAddress.Parse("ITGroup7843@gmtnigerialimited.onmicrosoft.com"));            
+            email.Cc.Add(MailboxAddress.Parse("ITGroup7843@gmtnigerialimited.onmicrosoft.com"));
+            //email.To.Add(MailboxAddress.Parse("fisayo.adegun@gmt-limited.com"));
+            //email.Cc.Add(MailboxAddress.Parse("francis.opogah@gmt-limited.com"));
             email.Subject = dailyCheck.Location == "Apapa" ? "DAILY CHECK" : dailyCheck.Location == "Abule-Oshun"
                 ? "OFFDOCK AND BMS DAILY CHECK" : "DAILY CHECK";
             var bodyBuilder = new BodyBuilder();
